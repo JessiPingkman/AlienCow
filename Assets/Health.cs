ad5754ba05869c932b6865ef583d7ac38c1c6390
Alymkulov Atai <ataii312@gmail.com>
2020-02-21 16:44:51 +0600

    add enemy movement

---
+++ Assets/Health.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float healthPoint;
+
+    public void СauseDamage(float damage)
+    {
+        healthPoint -= damage;
+        CheckHeath();
+    }
+
+    private void CheckHeath()
+    {
+        if (healthPoint <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+
+    private Rigidbody2D rigidbody2D;
+    private float startTime;
+    private float journeyLength;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(transform.position, target.position);
+    }
+
+    private void FixedUpdate()
+    {
+        MoveForward();
+    }
+
+    private void MoveForward()
+    {
+        float distCovered = (Time.time - startTime) * speed;
+        float fractionOfJourney = distCovered / journeyLength;
+        rigidbody2D.position = Vector2.Lerp(transform.position, target.position, fractionOfJourney);
+    }
+}
