68f3c974069b747dd2e3f3b14bf7643332785edc
Crudelis <ann.crudelis@gmail.com>
2020-02-24 12:46:58 +0600

    Added score system to save and load score

---
+++ Assets/Scripts/Score/Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int score;
+    Text scoreText;
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+    }
+
+    public void ScoreIncrement()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    public void SaveScore() 
+    {
+        SaveSystem.SaveScore();
+    }
+    public void LoadScore() 
+    {
+        ScoreData data = SaveSystem.LoadScore();
+        score = data.scoreData;
+        scoreText.text = score.ToString();
+    }
+
+}
---
+++ Assets/Scripts/Score/ScoreData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ScoreData 
+{
+    public int scoreData;
+
+    public ScoreData()
+    {
+        scoreData = Score.score;
+    }
+}
