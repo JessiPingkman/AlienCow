0094196e14cc3c230ce0e20310af6f007f32bac7
Nikita <jokerxxx99291488@gmail.com>
2020-02-24 18:01:14 +0600

    AudioManager done

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioSource audio;
+    private static Dictionary<string, AudioClip> sounds = new Dictionary<string, AudioClip>();
+
+    private void Awake()
+    {
+        audio.GetComponent<AudioSource>();
+        var soundsList = Resources.LoadAll<AudioClip>("/Sounds/");
+        foreach(var sound in soundsList)
+        {
+            sounds.Add(sound.name, sound);
+        }
+    }
+
+    public static void PlaySound(string name)
+    {
+        audio.PlayOneShot(sounds[name]);
+    }
+}
