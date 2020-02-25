a57d168a68f307622a3b7cc7155b8b9d8d3e5d39
Nikita <jokerxxx99291488@gmail.com>
2020-02-25 17:53:35 +0600

    fix name

--- Assets/Scripts/Rotatable.cs
+++ Assets/Scripts/Rotatable.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 
-public class Rotateble : MonoBehaviour
+public class Rotatable : MonoBehaviour
 {
     [SerializeField]
     private float _rotateSpeed;
 
-    private void Update()
+    private void FixedUpdate()
     {
         RotateToMousePosition();
     }
@@ -15,16 +15,16 @@ public class Rotateble : MonoBehaviour
         Vector2 direction = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
         float angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis (angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, _rotateSpeed * Time.deltaTime);
-        if(angle >= 10f)
-        {
-            Quaternion newRotation = Quaternion.AngleAxis(10f, Vector3.forward);
-            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, _rotateSpeed * Time.deltaTime);
-        }
-        else if(angle <= -50f)
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, _rotateSpeed);
+        if(angle <= -50f)
         {
             Quaternion newRotation = Quaternion.AngleAxis(-50f, Vector3.forward);
-            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, _rotateSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, _rotateSpeed);
+        }
+        else if(angle >= 6f)
+        {
+            Quaternion newRotation = Quaternion.AngleAxis(6f, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, _rotateSpeed);
         }
     }
 }
