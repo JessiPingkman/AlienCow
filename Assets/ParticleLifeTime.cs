ea4d2972832cdfc7cf7256c1dd0ab6ccf2cd9a9c
Alymkulov Atai <ataii312@gmail.com>
2020-02-27 14:31:22 +0600

    add limbs particle when enemy die

---
+++ Assets/ParticleLifeTime.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ParticleLifeTime : MonoBehaviour
+{
+    private ParticleSystem particle;
+    
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if(particle)
+        {
+            if(!particle.IsAlive())
+            {
+                ObjectPoolManager.Instance.ReturnToPool(PoolTags.EnemyLimbs, gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/Enums/PoolTags.cs
+++ Assets/Scripts/Enums/PoolTags.cs
@@ -2,5 +2,6 @@
 {
     Aliens,
     Pets,
-    ExplosionFx
+    ExplosionFx,
+    EnemyLimbs
 }
